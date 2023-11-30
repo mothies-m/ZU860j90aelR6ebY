392eb5d4fb18c7beeaeec0a44a9d9a79fc38416a
mothies-m <mothieswaran711@gmail.com>
2023-12-01 01:39:18 +0530

    typos

--- src/components/ResumeLink.jsx
+++ src/components/ResumeLink.jsx
@@ -4,8 +4,8 @@ import ResumePdf from '../assets/Resume.pdf'
 export default function ResumeLink() {
   return (
     <div>
-      <h1>Resume Link:</h1>
-      <a href={ResumePdf} download='my_resume.pdf'>gaw</a>
+      <h1>Résumé:</h1>
+      <a href={ResumePdf} download='my_resume.pdf'>Resume Link</a>
     </div>
   )
 }
