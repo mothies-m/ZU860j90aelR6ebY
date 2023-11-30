92ff3f9227144affa81f043b34382df9f29b190e
mothies-m <mothieswaran711@gmail.com>
2023-12-01 01:36:02 +0530

    data added

--- src/components/BmiCalculator.jsx
+++ src/components/BmiCalculator.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 export default function BmiCalculator() {
   return (
     <div>
-      <h1>BMI Calculator Link:</h1>
-      <Link to='/bmicalcuatorpage'></Link>
+      <h1>BMI Calculator:</h1>
+      <Link to='/bmicalcuatorpage'>BMI calculator link</Link>
     </div>
   )
 }
--- src/components/CandidateDetails.jsx
+++ src/components/CandidateDetails.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function CandidateDetails() {
   return (
-    <div>CandidateDetails</div>
+    <div>
+       <h1>Candidate Details</h1>
+        <ul>
+          <li>First Name:</li>
+          <p>Mothieswaran</p>
+          <li>Highest Degree Passout Year:</li>
+          <p>May/2025</p>
+          <li>Technologies/Language I have been working on:</li>
+            <p>Language - JavaScript, Python(current), Cpp, C.</p>
+            <p>FrameWork - React, NodeJS, Express.JS, Next.js(current).</p>
+        </ul>
+    </div>
   )
 }
