37123fe57b08222c5caffc7961b4e41472da3be9
mothies-m <mothieswaran711@gmail.com>
2023-12-01 01:36:40 +0530

    sections integrated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import ResumeLink from '../components/ResumeLink';
+import CandidateDetails from '../components/CandidateDetails';
+import BmiCalculator from '../components/BmiCalculator';
 
 export default function Home() {
+  let array=[<CandidateDetails/>, <ResumeLink/>, <BmiCalculator/>]
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      {
+        array.map((x) => {
+        return(
+      <section key={x}>{x}</section>
+        )})
+      }          
+    </div>
+  );
 }
